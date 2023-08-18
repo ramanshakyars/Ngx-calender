c982d8401b99cab80a4517cf1d9d582fa1f85e70
ramanshakyars <ramanshakyars12@gmail.com>
2023-08-18 16:54:14 +0530

    uploded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalenderComponent } from './components/calender/calender.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:CalenderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
